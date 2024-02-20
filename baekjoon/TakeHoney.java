16b55f9ac2c1ef8b8f1439ad7a13574fdb24e65e
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-02-20 18:27:03 +0900

    baekJoon 꿀따기너무시러

--- baekjoon/TakeHoney.java
+++ baekjoon/TakeHoney.java
@@ -6,18 +6,40 @@ public class TakeHoney {
     public static void main(String[] args) {
         int MAX = 10001;
         Scanner sc = new Scanner(System.in);
-        int[] Honey = new int[MAX];
-        int[] Sum = new int[MAX];
 
         int N = sc.nextInt();
+        int[] Honey = new int[MAX];
+        int[] Sum = new int[MAX];
+        int Answer = 0;
 
-        for(int i = 0; i < N; i++) {
+        for(int i = 1; i < N; i++) {
             Honey[i] = sc.nextInt();
-            Sum[i] = Sum[i-1] + Honey[i];
+            Sum[i] = Sum[i - 1] + Honey[i];
         }
 
-        // 벌꿀벌
-        // 꿀꿀벌
-        // 벌꿀꿀
+        for (int i = 2; i < N; i++) {
+            int cur = (Sum[i] - Sum[1]) + (Sum[N - 1] - Sum[i - 1]);
+            if(cur > Answer) {
+                Answer = cur;
+            }
+        }
+
+        // 2. 벌 - 벌 - 꿀통
+        for (int i = 2; i < N; i++) {
+            int cur = (Sum[N] - Sum[1] - Honey[i]) + (Sum[N] - Sum[i]);
+            if(cur > Answer) {
+                Answer = cur;
+            }
+        }
+
+        // 3. 꿀통 - 벌 - 벌
+        for (int i = 2; i < N; i++) {
+            int cur = (Sum[N - 1] - Honey[i]) + Sum[i - 1];
+            if(cur > Answer) {
+                Answer = cur;
+            }
+        }
+
+        System.out.println(Answer);
     }
 }
