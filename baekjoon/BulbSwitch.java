6c5fdc1ee6ee3d2dc81bc634bd7f1dd6813080db
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-02-26 18:29:41 +0900

    240226

--- baekjoon/BulbSwitch.java
+++ baekjoon/BulbSwitch.java
@@ -7,13 +7,41 @@ public class BulbSwitch {
         Scanner scanner = new Scanner(System.in);
 
         int num = scanner.nextInt();
-        int[][] bulbs = new int[2][num];
+        int[] bulbsStart = new int[num];
+        int[] bulbsFin = new int[num];
+        boolean[] bulbs = new boolean[num];
         int count = 0;
 
-        for(int i = 0; i < 2; i++) {
-            for(int j = 0; j < num; j++) {
-                bulbs[i][j] = scanner.nextInt();
+        for(int i = 0; i < num; i++) {
+            bulbsStart[i] = scanner.nextInt();
+        }
+        for(int i = 0; i < num; i++) {
+            bulbsFin[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < num; i++) {
+            if(bulbsStart[i] == bulbsFin[i]) {
+                bulbs[i] = true;
+            } else {
+                bulbs[i] = false;
             }
         }
+
+        for(int i = 0; i < num; i+= 3) {
+            if(bulbs[i] != bulbs[i + 2]) {
+                count++;
+            } else if (bulbs[i + 1]) {
+                if(!bulbs[i] && !bulbs[i + 2]) {
+                    count += 2;
+                }
+            } else {
+                if(bulbs[i] && bulbs[i + 2]) {
+                    count += 3;
+                } else if (bulbs[i] == bulbs[i + 1] || bulbs[i + 1] == bulbs[i + 2]) {
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
     }
 }
--- lotto/LottoMatching.java
+++ lotto/LottoMatching.java
@@ -1,21 +1,19 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class LottoMatching {
     /**
      * 로또 발급은 1 - 8까지 중복없는 6개 숫자. 하나는 구매한 로또이다.
      * 추가된 로직은 추첨된 로또이다.
      * 두 로또의 일치 여부에 따라 다음과 같은 메시지를 출력헌다.
-     *
      * 8개 숫자
      * 1등 - 6개 맞춤
      * 2등 - 5개 맞춤
      * 3등 - 4개 맞춤
      * 4등 - 3개 맞춤
      * 권외 - 2개 이하
-     *
      * 단, 구매로또 번호와 추첨 로또 번호 모두 같이 출력한다.
      */
 
@@ -39,26 +37,42 @@ public class LottoMatching {
         return arr;
     }
 
+    static int[] intArray(int[] arr) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<6; i++){
+            int num = sc.nextInt();
+            boolean check = false;
+            for(int j=0; j<6; j++){
+                if(arr[j] == num){
+                    // 중복되면  check 를 true 로 바꿔라..
+                    check = true;
+                }
+            }
+            if(check == false){
+                arr[i] = num; // 중복되지 않았으니 배열에 담아라.
+            }else{
+                i--; // 중복됐으면 이번 회수는 무효로 하고 다시 뽑아라.
+            }
+
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         int[] lottoA = new int[6];
         int[] lottoP = new int[6];
 
         lottoA = lotto(lottoA);
-        lottoP = lotto(lottoP);
+        lottoP = intArray(lottoP);
 
         Arrays.sort(lottoA);
 
+        System.out.print("정답: ");
         for(int i : lottoA) {
             System.out.print(i + " ");
         }
 
         System.out.print("\n");
-
-        Arrays.sort(lottoP);
-
-        for(int i: lottoP) {
-            System.out.print(i + " ");
-        }
 
         int count = 0;
 
